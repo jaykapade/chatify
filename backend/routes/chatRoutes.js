4e07d19a37e958e4ba473df86dc76994797e3a97
jazzdestructor <jazzdestructor@gmail.com>
2022-03-20 11:41:22 +0530

    Added More routes and controller functions for it

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,6 +4,9 @@ const {
   accessChat,
   fetchChats,
   createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
 } = require("../controllers/chatControllers");
 const router = express.Router();
 
@@ -12,8 +15,8 @@ router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup)
-// router.route("/groupremove").put(protect, removeFromGroup)
-// router.route("/groupadd").put(protect, addToGroup)
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
